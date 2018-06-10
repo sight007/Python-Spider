70658b668344009c53136feb44df770b887b8198
Mr.Gung <249053957@qq.com>
2018-06-10 15:29:35 +0800

    Add files via upload

---
+++ best University ranking/BestUniversities.py
@@ -0,0 +1,41 @@
+#coding:utf-8
+
+import requests
+from bs4 import BeautifulSoup
+import bs4
+
+#python 2.7支持中文
+#import sys
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url,timeout=30)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return ""
+
+def fillUnivList(ulist,html):
+    soup = BeautifulSoup(html,"html.parser")
+    for tr in soup.find("tbody").children:
+        if isinstance(tr, bs4.element.Tag):
+            tds = tr("td")
+            ulist.append([tds[0].string,tds[1].string,tds[3].string])
+
+def printUnivList(ulist,num):
+    tplt = "{0:^10}\t{1:{3}^10}\t{2:^10}"
+    print(tplt.format("排名","学校名称","总分",chr(12288)))
+    for u in ulist:
+        print(tplt.format(u[0],u[1],u[2],chr(12288)))
+
+def main():
+    uinfo = []
+    url = "http://www.zuihaodaxue.cn/zuihaodaxuepaiming2016.html"
+    html = getHTMLText(url)
+    fillUnivList(uinfo,html)
+    printUnivList(uinfo,100)
+
+main()
